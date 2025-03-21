176f4d016f82f433850363ff78d95f553ea03529
chukwunonso <onyewuchichukwunonso@gmail.com>
2025-03-21 23:24:17 +0100

    week 1

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/Users");
 
 // Get all users
 router.get("/", async (req, res) => {
